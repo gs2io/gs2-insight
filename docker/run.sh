e01da507e70c165c75ddc048b97c10fe57a1d6b6
Kazutomo Niwa <niwa_kazutomo@gs2.io>
2022-08-08 22:13:10 +0900

    Create .env for ECS

--- docker/run.sh
+++ docker/run.sh
@@ -2,13 +2,14 @@
 
 cd /var/www/html
 php ./composer.phar install
+
+mv .env.ecs .env
+
 php artisan migrate --force
 
 if test "$BIGQUERY_DATASET" != "" -a "$BIGQUERY_CREDENTIAL" != ""; then
     php artisan command:setup "$BIGQUERY_DATASET" "$BIGQUERY_CREDENTIAL" "`date -d '1weeks ago' +%Y-%m-%dT%H:%M:%S%z`" "`date +%Y-%m-%dT%H:%M:%S%z`"
     php artisan command:populate
 fi
-
-mv .env.ecs .env
 
 php artisan serve --host=0.0.0.0 --port=$PORT
